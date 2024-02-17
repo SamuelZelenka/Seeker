21df619f6c0b6604adf909353c00bab17e5490a9
Samuel Zelenka af Rolen <samuel.zelenka@hotmail.com>
2024-02-17 13:02:50 +0100

    Project structure cleanup

---
+++ Assets/Common/Code/Character/CharacterStates/ClimbState.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+
+public class ClimbState : ControllerState
+{
+	public ClimbState(ControllerState controllerState) : base(controllerState) 
+	{
+		Setup();
+	}
+
+	public ClimbState(
+		PlayerInput moveInfo,
+		PlayerController playerController,
+		CameraController cameraController,
+		CharacterConfig characterConfig,
+		CharacterController characterController) : base(
+		 moveInfo,
+		 characterConfig,
+		 playerController,
+		 characterController,
+		 cameraController)
+	{
+		Setup();
+	}
+
+	private void Setup()
+	{
+		_earlyActions.Add(Move);
+		_actions.Add(Jump);
+		_lateActions.Add(MoveCharacter);
+	}
+
+	public override float GetSpeedMultiplier(PlayerInput moveInfo, CharacterConfig CharConfig)
+	{
+		return CharConfig.ClimbSpeedMultiplier;
+	}
+
+	//TODO: fix climbing movement
+	public override void Move()
+	{
+		float hInput = _moveInfo.HorizontalInput;
+		float vInput = _moveInfo.VerticalInput;
+
+		Transform playerTrans = _playerController.transform;
+		Vector3 inputDir = playerTrans.right * hInput;
+
+				if (_moveInfo.IsGrounded)
+			inputDir += playerTrans.forward * vInput;
+
+		_playerController.velocity = Vector3.zero;
+
+		var climbVelocity = (_playerController.transform.forward.y + _cameraController.transform.forward.y + 0.2f) * _characterConfig.ClimbSpeedMultiplier;
+
+		if (vInput < 0)
+			_playerController.velocity.y += climbVelocity * vInput;
+		else
+			_playerController.velocity.y += climbVelocity * vInput;
+
+		var xSpeed = inputDir.x * _characterConfig.ClimbSpeedMultiplier;
+		var zSpeed = inputDir.z * _characterConfig.ClimbSpeedMultiplier;
+
+		_playerController.velocity.x += xSpeed;
+		_playerController.velocity.z += zSpeed;
+	}
+
+	public override void Jump()
+	{
+		if (_moveInfo.JumpInput == 0)
+			return;
+
+		var playerForward = _playerController.transform.forward;
+		var climbablePosition = _playerController.CurrentClimbable.transform.position;
+		var climbableDir = _playerController.transform.position - climbablePosition;
+		var dot = Vector3.Dot(playerForward, climbableDir);
+		var isFacingClimbable = dot < -0.3f;
+
+		if (isFacingClimbable)
+		{
+			_playerController.velocity += -_playerController.transform.forward * _characterConfig.JumpImpulse;
+		}
+		else
+		{
+			_playerController.velocity += _playerController.transform.forward * _characterConfig.JumpImpulse;
+		}
+	}
+}
---
+++ Assets/Config/CharacterConfig.cs
@@ -0,0 +1,102 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "CharacterConfig/Character Config", fileName = "New Character Config")]
+public class CharacterConfig : ScriptableObject
+{
+	[Header("External Forces")]
+	[SerializeField]
+	private float _airResistance;
+
+	public float AirResistance => _airResistance;
+
+	[SerializeField]
+	[Range(0f, 10f)]
+	private float _groundFriction;
+
+	public float GroundFriction => _groundFriction;
+
+	[Header("Acceleration")]
+	[SerializeField]
+	private float _airAcceleration;
+
+	public float AirAcceleration => _airAcceleration;
+
+	[SerializeField]
+	private float _groundAcceleration;
+
+	public float GroundAcceleration => _groundAcceleration;
+
+	[Header("Input Forces")]
+	[SerializeField]
+	private float _jumpImpulse;
+
+	public float JumpImpulse => _jumpImpulse;
+
+	[SerializeField]
+	private float _maxSpeed;
+
+	public float MaxSpeed => _maxSpeed;
+
+	[SerializeField]
+	[Range(0.1f, 1f)]
+	private float _forwardRate;
+
+	public float ForwardRate => _forwardRate;
+
+	[SerializeField]
+	[Range(0.1f, 1f)]
+	private float _backwardRate;
+
+	public float BackwardRate => _backwardRate;
+
+	[SerializeField]
+	[Range(0.1f, 1f)]
+	private float _sideRate;
+
+	public float SideRate => _sideRate;
+
+	[SerializeField]
+	[Range(0.1f, 1f)]
+	private float _climbSpeed;
+
+	public float ClimbSpeed => _climbSpeed;
+
+	[Header("Scaling")]
+	[SerializeField]
+	[Range(0.1f, 2)]
+	private float radius;
+	public float Radius => radius;
+
+	[SerializeField]
+	[Range(0.1f, 2f)]
+	private float _standHeight;
+
+	public float StandHeight => _standHeight;
+
+	[SerializeField]
+	[Range(0.1f, 2f)]
+	private float _crouchHeight;
+
+	public float CrouchHeight => _crouchHeight;
+
+	[Header("Speed")]
+	[SerializeField]
+	private float _runSpeedMultiplier = 2f;
+
+	public float RunSpeedMultiplier => _runSpeedMultiplier;
+
+	[SerializeField]
+	private float _crouchSpeedMultiplier = 0.8f;
+
+	public float CrouchSpeedMultiplier => _crouchSpeedMultiplier;
+
+	[SerializeField]
+	private float _walkSpeedMultiplier = 1f;
+
+	public float WalkSpeedMultiplier => _walkSpeedMultiplier;
+
+	[SerializeField]
+	private float _climbSpeedMultiplier = 1f;
+
+	public float ClimbSpeedMultiplier => _climbSpeedMultiplier;
+}
