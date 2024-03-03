d1d6a8d30ae6ef717d42af85d23cb4b7cd3e0de8
Samuel Zelenka af RolÃen <samuel.zelenka@hotmail.com>
2024-03-03 20:53:42 +0100

    Added Skill system

---
+++ Assets/Common/Code/Character/SkillController.cs
@@ -0,0 +1,73 @@
+﻿public class SkillController
+{
+	private SkillEvent _onNextSkill;
+	private SkillEvent _onPreviousSkill;
+	private SkillEvent _onSkillUsed;
+	private SkillEvent _onSkillChanged;
+
+	private int _activeSkillIndex;
+	private PlayerData _playerData;
+
+	public SkillController(PlayerData playerData, SkillEvent onNextSkill, SkillEvent onPreviousSkill, SkillEvent onSkillUsed, SkillEvent onSkillChanged)
+	{
+		_playerData = playerData;
+		_onNextSkill = onNextSkill;
+		_onPreviousSkill = onPreviousSkill;
+		_onSkillUsed = onSkillUsed;
+		_onSkillChanged = onSkillChanged;
+	}
+
+	public int GetActiveSkillsCount()
+	{
+		return _playerData.activeSkills.Length;
+	}
+
+	public int GetActiveSkillIndex()
+	{
+		return _activeSkillIndex;
+	}
+
+	public Skill GetSkillByIndex(int index)
+	{
+		return RepositoryManager.SkillRepository.GetSkill(_playerData.activeSkills[index]);
+	}
+
+	public Skill GetActiveSkill()
+	{
+		return GetSkillByIndex(_activeSkillIndex);
+	}
+
+	public Skill GetNextSkill()
+	{
+		var nextSkillIndex = MathExtra.WrapModulo(_activeSkillIndex + 1, _playerData.activeSkills.Length);
+
+		return RepositoryManager.SkillRepository.GetSkill(_playerData.activeSkills[nextSkillIndex]);
+	}
+
+	public Skill GetPreviousSkill()
+	{
+		var previousSkillIndex = MathExtra.WrapModulo(_activeSkillIndex - 1, _playerData.activeSkills.Length);
+
+		return RepositoryManager.SkillRepository.GetSkill(_playerData.activeSkills[previousSkillIndex]);
+	}
+
+	public void NextSkill()
+	{
+		_activeSkillIndex = MathExtra.WrapModulo(_activeSkillIndex + 1, _playerData.activeSkills.Length);
+		_onNextSkill?.Invoke(GetActiveSkill());
+		_onSkillChanged?.Invoke(GetActiveSkill());
+	}
+
+	public void PreviousSkill()
+	{
+		_activeSkillIndex = MathExtra.WrapModulo(_activeSkillIndex - 1, _playerData.activeSkills.Length);
+		_onPreviousSkill?.Invoke(GetActiveSkill());
+		_onSkillChanged?.Invoke(GetActiveSkill());
+	}
+
+	public void PerformActiveSkill()
+	{
+		GetActiveSkill().Perform();
+		_onSkillUsed.Invoke(GetActiveSkill());
+	}
+}
---
+++ Assets/Common/Code/Util/SkillEvent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+[CreateAssetMenu(menuName = "Events/Skill")]
+public class SkillEvent : ScriptableObject
+{
+	private Action<Skill> _event;
+
+	public void Subscribe(Action<Skill> func)
+	{
+		_event += func;
+	}
+
+	public void Unsubscribe(Action<Skill> func)
+	{
+		_event -= func;
+	}
+
+	public void Invoke(Skill skill)
+	{
+		_event?.Invoke(skill);
+	}
+}
